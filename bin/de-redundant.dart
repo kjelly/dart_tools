8d46803b1e52e9f5a4de1a40f5b2448f6f68b4db
Kuo-tung Kao <kjellytw@gmail.com>
2022-04-03 21:58:15 +0800

    Update

--- bin/de-redundant.dart
+++ bin/de-redundant.dart
@@ -46,12 +46,6 @@ Future main(List<String> args) async {
     exit(1);
   }
 
-  /* Logger.root.level = Level.WARNING; // defaults to Level.INFO */
-  Logger.root.onRecord.listen((record) {
-    print('${record.level.name}: ${record.time}: ${record.message}');
-  });
-  final log = Logger('main');
-
   final filePath = results['file'];
   final dictFilePath = results['dict'] ?? '';
   final threshold = int.tryParse(results['threshold']) ?? 80;
@@ -77,12 +71,7 @@ Future main(List<String> args) async {
     dict = File(dictFilePath).readAsLinesSync();
   }
   dict = dict.map((s) => s.toLowerCase()).toList();
-
-  EasyIsolate calRatioIsolate = EasyIsolate((args) {
-    var s = args[0] as String;
-    var t = args[1] as String;
-    return ratio(s, t);
-  }, worker: worker);
+  dict.shuffle();
 
   var lines = <String>[];
   if (filePath.isEmpty) {
@@ -97,11 +86,10 @@ Future main(List<String> args) async {
 
   var remain = lines.length;
 
-  Stream.fromIterable(lines).listen((oriLine) async {
-    log.info('start');
+  EasyIsolate removeRandomWordIsolate = EasyIsolate((args) {
     RegExp exp = RegExp(r'[a-zA-Z0-9]+');
     RegExp hasDigit = RegExp(r'[0-9]+');
-    var line = oriLine;
+    var line = args[0] as String;
     if (startColumn != 0) {
       line = line.split(' ').sublist(startColumn).join(' ');
     }
@@ -111,46 +99,54 @@ Future main(List<String> args) async {
     }
     var stripString = line;
     if (removeRandom) {
-      var replaceList =
-          await Stream.fromIterable(exp.allMatches(line)).asyncMap((m) {
+      for (var m in exp.allMatches(line)) {
         var matched = m.group(0)!.toLowerCase();
-        if (matched.toString().isEmpty) return '';
-        if (hasDigit.firstMatch(matched) != null) return '';
-        if (matched.length > 10) return '';
-        return (Stream<String>.fromIterable(dict).asyncMap((j) async {
-          var ret = calRatioIsolate.call([line, j]);
-          log.info('isolate');
-          return ret;
-        })).fold(0, (a, b) {
-          log.info('fold');
-          return max(a as int, b as int);
-        }).then((score) {
-          log.info('match');
-          if (score < randomThreshold) {
-            return matched;
+        if (matched.toString().isEmpty) continue;
+        if (hasDigit.firstMatch(matched) != null) {
+          stripString = stripString.replaceAll(matched, '');
+          continue;
+        }
+        if (matched.length > 10) {
+          stripString = stripString.replaceAll(matched, '');
+          continue;
+        }
+        var found = false;
+        for (var word in dict) {
+          if (ratio(matched, word) > randomThreshold) {
+            found = true;
+            break;
           }
-          return '';
-        });
-      }).toList();
-      for (var i in replaceList) {
-        var s = i as String;
-        if (s.isNotEmpty) {
-          stripString = stripString.replaceAll(s, '');
+        }
+        if (!found) {
+          stripString = stripString.replaceAll(matched, '');
         }
       }
     }
+    return stripString;
+  }, worker: worker);
 
-    log.info('mid1');
-    final score = await Stream<String>.fromIterable(List.from(stripData))
-        .asyncMap((j) async {
-      return (await calRatioIsolate.call([stripString, j]) as int);
-    }).fold(0, max);
-    log.info('end');
+  EasyIsolate deRedundantIsolate = EasyIsolate((args) {
+    var stripString = args[0] as String;
+    var stripData = args[1] as List<String>;
+    var threshold = args[2] as int;
 
-    if (score < threshold) {
+    var found = false;
+    for (var j in stripData) {
+      if (ratio(stripString, j) > threshold) {
+        found = true;
+        break;
+      }
+    }
+    return found;
+  }, worker: worker);
+
+  Stream.fromIterable(lines).listen((oriLine) async {
+    String stripString = await removeRandomWordIsolate.call([oriLine]);
+    if (!await deRedundantIsolate.call([stripString, stripData, threshold])) {
       data.add(oriLine);
       stripData.add(stripString);
     }
+
     remain--;
   });
 
@@ -158,7 +154,18 @@ Future main(List<String> args) async {
     await Future.delayed(Duration(milliseconds: 100));
     return remain > 0;
   });
-  await calRatioIsolate.close();
+  for (var i = stripData.length - 1; i >= 0; i--) {
+    for (var j = 0; j < i; j++) {
+      if (ratio(stripData[i], stripData[j]) > threshold) {
+        stripData.removeAt(i);
+        data.removeAt(i);
+        break;
+      }
+    }
+  }
+
+  await removeRandomWordIsolate.close();
+  await deRedundantIsolate.close();
 
   data.sort();
   for (var i in data) {
